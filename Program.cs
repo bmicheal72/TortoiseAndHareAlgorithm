8a3ff7408b70762b70cfce745aadb0f00052350f
bmicheal72 <132099228+bmicheal72@users.noreply.github.com>
2023-04-30 16:30:44 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ExampleProjTortoiseAndHareAlgorithm
+{
+    /* ------ Tortoise and Hare Algorithm ------ */
+
+    internal class Program
+    {
+        public static bool FloydCycleDetection<T>(LinkedList<T> list)
+        {
+            if(list.Count <= 2)
+            {
+                return false;
+            }
+
+            LinkedListNode<T> tortoise = list.First.Next;
+            LinkedListNode<T> hare = list.First.Next;
+
+            while(tortoise == hare)
+            {
+                if(tortoise == hare)
+                {
+                    return true;
+                }
+
+                if(hare.Next != null)
+                {
+                    hare = hare.Next.Next;
+                }
+
+                tortoise = tortoise.Next;
+            }
+
+            return false;
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
